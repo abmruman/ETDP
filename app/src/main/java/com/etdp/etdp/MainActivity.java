9bdde9165e36c5f398fe0c98cd9ba1d90cb5ac4e
A B M Ruman <abm.ruman@gmail.com>
2016-10-25 14:54:15 +0600

    Add Basic MainActivity with fragment

---
+++ app/src/main/java/com/etdp/etdp/MainActivity.java
@@ -0,0 +1,33 @@
+package com.etdp.etdp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView textView = (TextView) findViewById(R.id.textView);
+                textView.setText("Starting...");
+                Snackbar.make(view, "Starting Services...", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/etdp/etdp/MainActivityFragment.java
@@ -0,0 +1,31 @@
+package com.etdp.etdp;
+
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Snackbar.make(getView(), "Fragment started", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
