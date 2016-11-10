0c27fcf11e2241e673872b5dbe2670c09084c474
A B M Ruman <abm.ruman@gmail.com>
2016-11-10 16:40:31 +0600

    Add ETDPService and implement start, stop

--- app/src/main/java/com/etdp/etdp/GeoLocationActivity.java
+++ app/src/main/java/com/etdp/etdp/GeoLocationActivity.java
@@ -8,224 +8,251 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
+import com.etdp.etdp.services.ETDPService;
 
 import java.util.List;
 
 import pub.devrel.easypermissions.AfterPermissionGranted;
 import pub.devrel.easypermissions.EasyPermissions;
 
-public class GeoLocationActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks{
+public class GeoLocationActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks {
 
-    static final int REQUEST_PERMISSION_ACCESS_FINE_LOCATION = 1004;
-    // Acquire a reference to the system Location Manager
-    private LocationManager locationManager;
-    // Define a listener that responds to location updates
-    private LocationListener locationListener;
+	static final int REQUEST_PERMISSION_ACCESS_FINE_LOCATION = 1004;
+	// Acquire a reference to the system Location Manager
+	private LocationManager locationManager;
+	// Define a listener that responds to location updates
+	private LocationListener locationListener;
 
-    private static final int TWO_MINUTES = 1000 * 60 * 2;
+	private static final int TWO_MINUTES = 1000 * 60 * 2;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-        // Register the listener with the Location Manager to receive location updates
-        /** Location Manager & Listener **/
-        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-        locationListener = new LocationListener() {
-            public void onLocationChanged(Location location) {
-                // Called when a new location is found by the network location provider.
-                useNewLocation(location);
-            }
+		// Register the listener with the Location Manager to receive location updates
+		/** Location Manager & Listener **/
+		locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+		locationListener = new LocationListener() {
+			public void onLocationChanged(Location location) {
+				// Called when a new location is found by the network location provider.
+				useNewLocation(location);
+			}
 
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-            }
+			public void onStatusChanged(String provider, int status, Bundle extras) {
+			}
 
-            public void onProviderEnabled(String provider) {
-            }
+			public void onProviderEnabled(String provider) {
+			}
 
-            public void onProviderDisabled(String provider) {
-            }
-        };
+			public void onProviderDisabled(String provider) {
+			}
+		};
 
-        // Register the listener with the Location Manager to receive location updates
-        if (ActivityCompat.checkSelfPermission(
-                this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults);
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            checkPermission();
-            return;
-        }
-        statusCheck();
-        locationManager.requestLocationUpdates(locationManager.NETWORK_PROVIDER, 0, 0, locationListener) ;
+		// Register the listener with the Location Manager to receive location updates
+		if (ActivityCompat.checkSelfPermission(
+				this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+			// TODO: Consider calling
+			//    ActivityCompat#requestPermissions
+			// here to request the missing permissions, and then overriding
+			//   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+			//                                          int[] grantResults);
+			// to handle the case where the user grants the permission. See the documentation
+			// for ActivityCompat#requestPermissions for more details.
+			checkPermission();
+			return;
+		}
+		statusCheck();
+		locationManager.requestLocationUpdates(locationManager.NETWORK_PROVIDER, 0, 0, locationListener);
 
-        setContentView(R.layout.activity_geo_location);
-    }
+		setContentView(R.layout.activity_geo_location);
+	}
 
-    private boolean startLitening() {
+	private boolean startLitening() {
 
-        String locationProvider = LocationManager.NETWORK_PROVIDER;
-        // Or, use GPS location data:
-         // String locationProvider = LocationManager.GPS_PROVIDER;
+		String locationProvider = LocationManager.NETWORK_PROVIDER;
+		// Or, use GPS location data:
+		// String locationProvider = LocationManager.GPS_PROVIDER;
 
-        if (ActivityCompat.checkSelfPermission(
-                this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            checkPermission();
-            return false;
-        }
-        statusCheck();
-        locationManager.requestLocationUpdates(locationProvider, 0, 0, locationListener);
-        return true;
-    }
+		if (ActivityCompat.checkSelfPermission(
+				this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+			// TODO: Consider calling
+			//    ActivityCompat#requestPermissions
+			// here to request the missing permissions, and then overriding
+			//   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+			//                                          int[] grantResults)
+			// to handle the case where the user grants the permission. See the documentation
+			// for ActivityCompat#requestPermissions for more details.
+			checkPermission();
+			return false;
+		}
+		statusCheck();
+		locationManager.requestLocationUpdates(locationProvider, 0, 0, locationListener);
+		return true;
+	}
 
-    private Location lastKnownLocation() {
-        String locationProvider = LocationManager.NETWORK_PROVIDER;
-        // Or use LocationManager.GPS_PROVIDER
-        Location lastKnownLocation;
+	private Location lastKnownLocation() {
+		String locationProvider = LocationManager.NETWORK_PROVIDER;
+		// Or use LocationManager.GPS_PROVIDER
+		Location lastKnownLocation;
 
-        if (ActivityCompat.checkSelfPermission(
-                this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            checkPermission();
-            return null;
-        }
-        statusCheck();
-        return locationManager.getLastKnownLocation(locationProvider);
-    }
+		if (ActivityCompat.checkSelfPermission(
+				this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+			// TODO: Consider calling
+			//    ActivityCompat#requestPermissions
+			// here to request the missing permissions, and then overriding
+			//   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+			//                                          int[] grantResults)
+			// to handle the case where the user grants the permission. See the documentation
+			// for ActivityCompat#requestPermissions for more details.
+			checkPermission();
+			return null;
+		}
+		statusCheck();
+		return locationManager.getLastKnownLocation(locationProvider);
+	}
 
 
-    /** Determines whether one Location reading is better than the current Location fix
-     * @param location  The new Location that you want to evaluate
-     * @param currentBestLocation  The current Location fix, to which you want to compare the new one
-     */
-    protected boolean isBetterLocation(Location location, Location currentBestLocation) {
-        if (currentBestLocation == null) {
-            // A new location is always better than no location
-            return true;
-        }
+	/**
+	 * Determines whether one Location reading is better than the current Location fix
+	 *
+	 * @param location            The new Location that you want to evaluate
+	 * @param currentBestLocation The current Location fix, to which you want to compare the new one
+	 */
+	protected boolean isBetterLocation(Location location, Location currentBestLocation) {
+		if (currentBestLocation == null) {
+			// A new location is always better than no location
+			return true;
+		}
 
-        // Check whether the new location fix is newer or older
-        long timeDelta = location.getTime() - currentBestLocation.getTime();
-        boolean isSignificantlyNewer = timeDelta > TWO_MINUTES;
-        boolean isSignificantlyOlder = timeDelta < -TWO_MINUTES;
-        boolean isNewer = timeDelta > 0;
+		// Check whether the new location fix is newer or older
+		long timeDelta = location.getTime() - currentBestLocation.getTime();
+		boolean isSignificantlyNewer = timeDelta > TWO_MINUTES;
+		boolean isSignificantlyOlder = timeDelta < -TWO_MINUTES;
+		boolean isNewer = timeDelta > 0;
 
-        // If it's been more than two minutes since the current location, use the new location
-        // because the user has likely moved
-        if (isSignificantlyNewer) {
-            return true;
-            // If the new location is more than two minutes older, it must be worse
-        } else if (isSignificantlyOlder) {
-            return false;
-        }
+		// If it's been more than two minutes since the current location, use the new location
+		// because the user has likely moved
+		if (isSignificantlyNewer) {
+			return true;
+			// If the new location is more than two minutes older, it must be worse
+		} else if (isSignificantlyOlder) {
+			return false;
+		}
 
-        // Check whether the new location fix is more or less accurate
-        int accuracyDelta = (int) (location.getAccuracy() - currentBestLocation.getAccuracy());
-        boolean isLessAccurate = accuracyDelta > 0;
-        boolean isMoreAccurate = accuracyDelta < 0;
-        boolean isSignificantlyLessAccurate = accuracyDelta > 200;
+		// Check whether the new location fix is more or less accurate
+		int accuracyDelta = (int) (location.getAccuracy() - currentBestLocation.getAccuracy());
+		boolean isLessAccurate = accuracyDelta > 0;
+		boolean isMoreAccurate = accuracyDelta < 0;
+		boolean isSignificantlyLessAccurate = accuracyDelta > 200;
 
-        // Check if the old and new location are from the same provider
-        boolean isFromSameProvider = isSameProvider(location.getProvider(),
-                currentBestLocation.getProvider());
+		// Check if the old and new location are from the same provider
+		boolean isFromSameProvider = isSameProvider(location.getProvider(),
+				currentBestLocation.getProvider());
 
-        // Determine location quality using a combination of timeliness and accuracy
-        if (isMoreAccurate) {
-            return true;
-        } else if (isNewer && !isLessAccurate) {
-            return true;
-        } else if (isNewer && !isSignificantlyLessAccurate && isFromSameProvider) {
-            return true;
-        }
-        return false;
-    }
+		// Determine location quality using a combination of timeliness and accuracy
+		if (isMoreAccurate) {
+			return true;
+		} else if (isNewer && !isLessAccurate) {
+			return true;
+		} else if (isNewer && !isSignificantlyLessAccurate && isFromSameProvider) {
+			return true;
+		}
+		return false;
+	}
 
-    /** Checks whether two providers are the same */
-    private boolean isSameProvider(String provider1, String provider2) {
-        if (provider1 == null) {
-            return provider2 == null;
-        }
-        return provider1.equals(provider2);
-    }
+	/**
+	 * Checks whether two providers are the same
+	 */
+	private boolean isSameProvider(String provider1, String provider2) {
+		if (provider1 == null) {
+			return provider2 == null;
+		}
+		return provider1.equals(provider2);
+	}
 
-    private void useNewLocation(Location location) {
-        // TODO: 10-Oct-16 Test Location service
+	private void useNewLocation(Location location) {
+		// TODO: 10-Oct-16 Test Location service
 
-        Toast.makeText(this, "ETDP: "+location.toString(), Toast.LENGTH_LONG).show();
-    }
+		Toast.makeText(this, "ETDP: " + location.toString(), Toast.LENGTH_LONG).show();
+	}
 
-    @Override
-    public void onPermissionsGranted(int requestCode, List<String> perms) {
+	@Override
+	public void onPermissionsGranted(int requestCode, List<String> perms) {
 
-    }
+	}
 
-    @Override
-    public void onPermissionsDenied(int requestCode, List<String> perms) {
+	@Override
+	public void onPermissionsDenied(int requestCode, List<String> perms) {
 
-    }
+	}
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        EasyPermissions.onRequestPermissionsResult(
-                requestCode, permissions, grantResults, this);
-    }
+	@Override
+	public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+		super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+		EasyPermissions.onRequestPermissionsResult(
+				requestCode, permissions, grantResults, this);
+	}
 
-    public void statusCheck() {
-        //final LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+	public void statusCheck() {
+		//final LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
-        if (!locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
-            buildAlertMessageNoGps();
-        }
-    }
+		if (!locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+			buildAlertMessageNoGps();
+		}
+	}
 
-    private void buildAlertMessageNoGps() {
-        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage("Your GPS seems to be disabled, do you want to enable it?")
-                .setCancelable(false)
-                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                    public void onClick(final DialogInterface dialog, final int id) {
-                        startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
-                    }
-                })
-                .setNegativeButton("No", new DialogInterface.OnClickListener() {
-                    public void onClick(final DialogInterface dialog, final int id) {
-                        dialog.cancel();
-                    }
-                });
-        final AlertDialog alert = builder.create();
-        alert.show();
-    }
+	private void buildAlertMessageNoGps() {
+		final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setMessage("Your GPS seems to be disabled, do you want to enable it?")
+				.setCancelable(false)
+				.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+					public void onClick(final DialogInterface dialog, final int id) {
+						startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+					}
+				})
+				.setNegativeButton("No", new DialogInterface.OnClickListener() {
+					public void onClick(final DialogInterface dialog, final int id) {
+						dialog.cancel();
+					}
+				});
+		final AlertDialog alert = builder.create();
+		alert.show();
+	}
 
-    @AfterPermissionGranted(REQUEST_PERMISSION_ACCESS_FINE_LOCATION)
-    private void checkPermission() {
-        if(!EasyPermissions.hasPermissions(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
-            EasyPermissions.requestPermissions(this, "This app needs to access your location", REQUEST_PERMISSION_ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION);
-        }
-    }
+	@AfterPermissionGranted(REQUEST_PERMISSION_ACCESS_FINE_LOCATION)
+	private void checkPermission() {
+		if (!EasyPermissions.hasPermissions(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+			EasyPermissions.requestPermissions(this, "This app needs to access your location", REQUEST_PERMISSION_ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION);
+		}
+	}
+
+
+	public void startMainService() {
+		Intent intent = new Intent(this, ETDPService.class);
+		intent.putExtra("Source", "MainActivity");
+		startService(intent);
+	}
+
+	public void stopMainService() {
+		Intent intent = new Intent(this, ETDPService.class);
+		intent.putExtra("Source", "MainActivity");
+		stopService(intent);
+	}
+
+	public void startMainService(View view) {
+		startMainService();
+	}
+
+	public void stopMainService(View view) {
+		stopMainService();
+	}
+
 }
